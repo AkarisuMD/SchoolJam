a56eb49acfe2811c1a7063ad4d7b9d8956b19338
AkarisuMD <91060309+AkarisuMD@users.noreply.github.com>
2023-05-05 14:16:13 +0200

    AAA

--- Assets/Scripts/BriqueDeGameplay.cs
+++ Assets/Scripts/BriqueDeGameplay.cs
@@ -9,6 +9,5 @@ public class BriqueDeGameplay : MonoBehaviour
     private void OnMouseUp()
     {
         pickleBrick.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-7f, -5f, 12f), ForceMode.Impulse);
-        Destroy(this.gameObject);
     }
 }
